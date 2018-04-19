c6cf0ad33a8cc7ecf5ccc1d4b2f82aa72b394ad2
Teresa Havlicek <teresahavlicek@teresas-mbp.codefactoryvienna.local>
2018-04-19 09:45:48 +0200

    first exercises

---
+++ Basic Exercises/test.js
@@ -0,0 +1,9 @@
+//var age = 25;
+
+//document.write("Hi my name is Jeremy" + " " + "and I am " + age + " years old")
+
+
+players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+
+document.write("The most valuable player of the match is " + players[2]);
+ 
