2eacbb56a132aaf84eae9d76498047775297993e
Teresa Havlicek <teresahavlicek@teresas-mbp.codefactoryvienna.local>
2018-04-19 11:52:55 +0200

    commit

---
+++ Advanced Exercises/script.js
@@ -0,0 +1,104 @@
+//Exercise Sum 
+
+var a = 7;
+
+var b = 14;
+
+var c = "21";
+
+var d = '36';
+
+var e = 42;
+
+
+/*var temp = Number(c);
+
+var temp2 = Number(d);
+
+
+console.log(temp);
+
+console.log(temp2);*/
+
+var sum = a + b + Number(c) + Number(d) + e;
+
+console.log(sum);
+
+//Exercise multiplication 
+
+
+var f = '1';
+
+var g = 15;
+
+var h = 8;
+
+var i = "1";
+
+var multi = Number(1) * g * 8 * Number(i);
+
+console.log(multi);
+
+//Division 
+
+/*var div = multi/sum;
+document.write(div);*/
+
+
+
+//Exercise 2
+
+
+var numbers = [ 
+[1, 2, 1, 24], 
+[8, 11, 9, 4], 
+[7, 0, 7, 27], 
+[7, 4, 28, 14], 
+[3, 10, 25, 7], 
+[21, 4, 6, 17], 
+[3, 5, 26, 3] 
+]
+
+
+document.write(numbers[1][1]+" " +numbers[4][2]+" " +numbers[5][3]+" " +numbers[2][3]+" " +numbers[2][1]);
+
+//Exercise 3 String variation 
+
+/*var V= "With the online map of Vienna you can easily navigate through Vienna."
+
+var temp4 = V.substring (23,29);
+
+var temp5 = V.substring (62,68);
+
+
+document.write("<br>");
+
+
+console.log(temp4+" "+temp5);
+
+document.write(temp4+" "+temp5);*/
+
+
+
+var Vienna= "With the online map of Vienna you can easily navigate through Vienna.";
+
+/*function myFunction() {
+    Vienna;
+    document.getElementById("Vienna").innerHTML = str.repeat(2);
+}*/
+
+var temp6= V.word.match("Vienna");
+
+
+console.log (temp6);
+
+
+
+
+
+
+
+
+
+
+
