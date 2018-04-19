614ca7613066624711bf565bea1239065abf8d54
DominicWieser <dominic.wieser1995@live.at>
2018-04-19 13:52:33 +0200

    adding first part of the challange

---
+++ Challenge/script.js
@@ -0,0 +1,18 @@
+
+var multi = [
+['Vienna', ' ', ' ', ' ' ,' '], 
+['\u00a0', 'is', ' ', ' ' ,' '],
+['\u00a0', '\u00a0', 'a', ' ' ,' '],
+['\u00a0', '\u00a0', '\u00a0', 'nice' ,' '], 
+['\u00a0', '\u00a0', '\u00a0', '\u00a0' ,'city']
+];
+
+document.write(multi[0][0] + ' ' + multi[0][1] + ' ' + multi[0][2] + ' ' + multi[0][3] + ' ' + multi[0][4]);
+document.write('<br>');
+document.write(multi[1][0] + ' ' + multi[1][1] + ' ' + multi[1][2] + ' ' + multi[1][3] + ' ' + multi[1][4]);
+document.write('<br>');
+document.write(multi[2][0] + ' ' + multi[2][1] + ' ' + multi[2][2] + ' ' + multi[2][3] + ' ' + multi[2][4]);
+document.write('<br>');
+document.write(multi[3][0] + ' ' + multi[3][1] + ' ' + multi[3][2] + ' ' + multi[3][3] + ' ' + multi[3][4]);
+document.write('<br>');
+document.write(multi[4][0] + ' ' + multi[4][1] + ' ' + multi[4][2] + ' ' + multi[4][3] + ' ' + multi[4][4]);
